680973785ff7cfe5e9e9cd5779100e59c1f0c138
zacher tiburzi <zacher.t@comcast.net>
2016-09-27 12:48:29 -0700

    Update palindrome.py

--- Palindrome-Verify/palindrome.py
+++ Palindrome-Verify/palindrome.py
@@ -1,1 +1,36 @@
 
+#!/usr/bin/env python -tt
+""" File: palindrome.py """
+
+
+"""Summary line: Checks if a string is a palindrome.
+		
+Description: This program will take a string input from a user and 
+	will return true if the string is a palindrome. 
+"""
+
+
+print "\nThis program determines if a phrase is a palindrome."
+print "Type ^c to exit.\n"
+
+
+while True:
+	try:
+		phrase 	      =  raw_input("Please enter a phrase: " )
+		phrase_Concat =  ''.join(phrase.split(' '))
+	
+	except Exception as KeyboardInterrupt:
+		print("\nClosing the application. \n")
+		break
+		
+	else:
+		if phrase_Concat == phrase_Concat[::-1]:
+			print "Yes!, \"%s\" is a palindrome.\n" % (phrase)
+
+		else:
+			print "No!, \"%s\" is not a palindrome.\n" % (phrase)
+
+			
+		
+		
+		
