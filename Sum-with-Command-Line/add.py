fb166b0f0764486ea7d2f7dfd0d0b8b952edd4a6
zacher tiburzi <zacher.t@comcast.net>
2016-09-27 13:26:13 -0700

    Update add.py

--- Sum-with-Command-Line/add.py
+++ Sum-with-Command-Line/add.py
@@ -1,1 +1,42 @@
 
+#!/usr/bin/env python -tt
+""" File: pascal.py """
+
+
+def isFloat(value):
+    """Summary line: Returns true if the value is a float, and else false
+
+    Description: If the value passed can be a float the a true is returned, and 
+        if not an exception is raised and false is returned
+    """
+
+    try:
+        float(value)
+        return True
+
+    except ValueError:
+        return False
+
+
+
+
+if __name__ == '__main__':
+    """Summary line: Calculates the sum of a list of values
+
+    Description: This program uses the sys package to capture command
+        line arguments, filters out the non digits, and retiurns the 
+        sum of all the values.
+    """
+
+    from   sys import argv
+    import re
+
+
+    values      = argv
+    values_sum  = [float(_) for _ in values if _.isdigit() or isFloat(_)]
+
+    print  sum(values_sum)
+
+
+
+
