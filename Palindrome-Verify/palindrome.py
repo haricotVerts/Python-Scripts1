6cab4d819bc740f09252ebf40ecd02010534f0b3
zacher tiburzi <zacher.t@comcast.net>
2016-09-27 13:01:44 -0700

    Update palindrome.py

--- Palindrome-Verify/palindrome.py
+++ Palindrome-Verify/palindrome.py
@@ -1,12 +1,11 @@
-
 #!/usr/bin/env python -tt
 """ File: palindrome.py """
 
 
 """Summary line: Checks if a string is a palindrome.
-		
+
 Description: This program will take a string input from a user and 
-	will return true if the string is a palindrome. 
+    will return true if the string is a palindrome. 
 """
 
 
@@ -15,22 +14,20 @@ print "Type ^c to exit.\n"
 
 
 while True:
-	try:
-		phrase 	      =  raw_input("Please enter a phrase: " )
-		phrase_Concat =  ''.join(phrase.split(' '))
-	
-	except Exception as KeyboardInterrupt:
-		print("\nClosing the application. \n")
-		break
-		
-	else:
-		if phrase_Concat == phrase_Concat[::-1]:
-			print "Yes!, \"%s\" is a palindrome.\n" % (phrase)
+    try:
+        phrase 	      =  raw_input("Please enter a phrase: " )
+        phrase_Concat =  ''.join(phrase.split(' '))
 
-		else:
-			print "No!, \"%s\" is not a palindrome.\n" % (phrase)
+    except Exception as KeyboardInterrupt:
+        print("\nClosing the application. \n")
+        break
 
-			
-		
-		
-		
+    else:
+        if phrase_Concat == phrase_Concat[::-1]:
+            print "Yes!, \"%s\" is a palindrome.\n" % (phrase)
+
+        else:
+            print "No!, \"%s\" is not a palindrome.\n" % (phrase)
+
+
+
